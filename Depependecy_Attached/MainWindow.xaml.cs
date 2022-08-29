373546158fe2267bf9b6043ac5630833eeaf9f16
HlKun <1104736831@qq.com>
2022-08-29 21:45:51 +0800

    commit

---
+++ Depependecy_Attached/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Depependecy_Attached
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Human human = new Human();
+            School.SetGrade(human, 1);
+        }
+    }
+
+    public class School
+    {
+        public static int GetGrade(DependencyObject obj)
+        {
+            return (int)obj.GetValue(GradeProperty);
+        }
+
+        public static void SetGrade(DependencyObject obj, int value)
+        {
+            obj.SetValue(GradeProperty, value);
+        }
+
+        public static readonly DependencyProperty GradeProperty =
+            DependencyProperty.RegisterAttached("Grade", typeof(int), typeof(School), new PropertyMetadata(0));
+    }
+
+    public class Human : DependencyObject
+    {
+        
+    }
+}
---
+++ Event_WPF/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Event_WPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, ReportTimeEventArgs e)
+        {
+
+        }
+    }
+
+    public class ReportTimeEventArgs : RoutedEventArgs
+    {
+        public ReportTimeEventArgs(RoutedEvent routedEvent, object source)
+            : base(routedEvent, source) { }
+
+        public DateTime ClickTime { get; set; }
+    }
+
+    public class TimeButotn : Button
+    {
+        public static readonly RoutedEvent ReportTimeEvent = EventManager.RegisterRoutedEvent
+            ("ReportTime", RoutingStrategy.Bubble, typeof(EventHandler<ReportTimeEventArgs>), typeof(TimeButotn));
+
+        public event RoutedEventHandler ReportTime
+        {
+            add { AddHandler(ReportTimeEvent, value); }
+            remove { RemoveHandler(ReportTimeEvent, value); }
+        }
+
+        protected override void OnClick()
+        {
+            base.OnClick();
+
+            ReportTimeEventArgs args = new ReportTimeEventArgs(ReportTimeEvent, this)
+            {
+                ClickTime = DateTime.Now
+            };
+            RaiseEvent(args);
+        }
+    }
+}
