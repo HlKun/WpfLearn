86716379ae36699b5146095b9d9a2c68687ee2c4
HlKun <1104736831@qq.com>
2022-09-09 10:58:40 +0800

    commit “Bridge”

---
+++ DesignPattern/Structure/Bridge.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Structure
+{
+    #region Bridge 组合替换继承
+    public interface IBrand
+    {
+
+    }
+
+    public interface IVariety
+    {
+
+    }
+
+    public class Computer
+    {
+        private readonly IBrand brand;
+        private readonly IVariety variety;
+
+        public Computer(IBrand _brand, IVariety _variety)
+        {
+            brand = _brand;
+            variety = _variety;
+        }
+    }
+    #endregion
+
+    #region 继承模式
+    public class BaseComputer { }
+
+    public class DellComputer : BaseComputer { }
+
+    public class DellDesktopComputer : DellComputer { }
+
+    public class LenovoCompter : BaseComputer { }
+
+    public class LenovoDesktopComputer : LenovoCompter { }
+    #endregion
+}
