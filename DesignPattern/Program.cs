9afe7901975a1daba1be33338c9b55fdea12c935
HlKun <1104736831@qq.com>
2022-09-21 11:40:04 +0800

    commit  “Compsite”

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -6,12 +6,20 @@ namespace DesignPattern
     {
         static void Main(string[] args)
         {
+            #region Dectorator
             People china = new China();
 
             var newEnglish = new English(china);
             var newFrance = new France(newEnglish);
 
             newFrance.Say();
+            #endregion
+
+            #region Composite
+            Composite composite = new Composite();
+            composite.Add(new Leaf());
+            composite.Add(new Composite());
+            #endregion
         }
     }
 }
---
+++ DesignPattern/Structure/Composite.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Structure
+{
+    public abstract class Component
+    {
+        public abstract void Add(Component component);
+
+        public abstract void Remove(Component component);
+
+        public abstract void Draw();
+    }
+
+    public class Composite : Component
+    {
+        private readonly List<Component> Children = new List<Component>();
+
+        public override void Add(Component component)
+        {
+            if (component != null)
+                Children.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            if (component != null)
+                Children.Add(component);
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Draw");
+        }
+    }
+
+    public class Leaf : Component
+    {
+        public override void Add(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Remove(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Draw");
+        }
+    }
+}
