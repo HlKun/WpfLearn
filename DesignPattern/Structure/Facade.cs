0e4d01027200f427558fc0a05cb27fe95ebed21c
HlKun <1104736831@qq.com>
2022-09-22 10:13:20 +0800

    Create Facade.cs

---
+++ DesignPattern/Structure/Facade.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Structure
+{
+    public class TV
+    {
+        public void On()
+        {
+            Console.WriteLine("TV ON");
+        }
+    }
+
+    public class Light
+    {
+        public void On()
+        {
+            Console.WriteLine("Light On");
+        }
+    }
+
+    /// <summary>
+    /// 外观模式
+    /// </summary>
+    public class Facade
+    {
+        private readonly TV tV;
+        private readonly Light light;
+
+        public Facade(TV _tV, Light _light)
+        {
+            tV = _tV;
+            light = _light;
+        }
+
+        public void On()
+        {
+            tV.On();
+            light.On();
+        }
+    }
+}
