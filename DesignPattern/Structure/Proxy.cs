8c2b488ffa938291d7fee51b3ab844dc6aa96e47
HlKun <1104736831@qq.com>
2022-09-26 10:46:04 +0800

    commit “Proxy”

---
+++ DesignPattern/Structure/Proxy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Structure
+{
+    public abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    public class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Real Request....");
+        }
+    }
+
+
+    /// <summary>
+    /// 代理模式
+    /// </summary>
+    public class Proxy : Subject
+    {
+        private readonly Subject subject;
+
+        public Proxy()
+        {
+            subject = new RealSubject();
+        }
+
+        public override void Request()
+        {
+            Console.WriteLine("Proxy Request....");
+            subject.Request();
+        }
+    }
+}
