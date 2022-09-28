1817a18fb2224430c35e963c54cd834ec0f22dec
HlKun <1104736831@qq.com>
2022-09-28 11:13:05 +0800

    commit “Command”

---
+++ DesignPattern/Behavior/Command.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Behavior
+{
+    /// <summary>
+    /// 命令模式
+    /// </summary>
+    public abstract class Command
+    {
+        public abstract void Execute();
+    }
+
+    public class TurnOnTVCommand : Command
+    {
+        private readonly TV tv;
+
+        public TurnOnTVCommand(TV _tv)
+        {
+            tv = _tv;
+        }
+
+        public override void Execute()
+        {
+            tv.TurnOn();
+        }
+    }
+
+    /// <summary>
+    /// Receiver
+    /// </summary>
+    public class TV
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Turn on TV");
+        }
+    }
+
+    public class Invoker
+    {
+        private readonly Command turnOnTV;
+
+        public Invoker(Command _turnOnTV)
+        {
+            turnOnTV = _turnOnTV;
+        }
+
+        public void ExecuteCommand()
+        {
+            turnOnTV.Execute();
+        }
+    }
+}
